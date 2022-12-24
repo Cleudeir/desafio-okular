40960974897be43837175ab80827ba0f0b2bc3d3
cleudeir <cleudeirsilva@gmail.com>
2022-12-24 07:38:00 -0300

    opacity control video

---
+++ components/ListBarVideo.js
@@ -0,0 +1,12 @@
+import styles from '../styles/VideoContainer.module.css'
+
+const ListBarVideo = ({ }) => {
+
+    return (
+        <div className={styles.container}>
+
+        </div >
+    );
+};
+
+export default ListBarVideo;
