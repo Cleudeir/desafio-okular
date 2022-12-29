15b05d218c97bdde4ca61947604f9325ccfa72a9
Cleudeir <Cleudeirsilva@gmail.com>
2022-12-28 23:17:31 -0300

    full screen debug

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "/styles/Index.module.css";
 import VideoContainer from "../components/VideoContainer";
 import ListBarVideo from "../components/ListBarVideo";
